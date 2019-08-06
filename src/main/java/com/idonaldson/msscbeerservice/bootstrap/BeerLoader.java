905bb850aea8321d8c601333d325cf0283f66e04
Ian Donaldson <ian.donaldson@ellucian.com>
2019-08-06 16:34:43 -0500

    Fixed tests. closes #11

--- src/main/java/com/idonaldson/msscbeerservice/bootstrap/BeerLoader.java
+++ src/main/java/com/idonaldson/msscbeerservice/bootstrap/BeerLoader.java
@@ -2,11 +2,13 @@ package com.idonaldson.msscbeerservice.bootstrap;
 
 import com.idonaldson.msscbeerservice.domain.Beer;
 import com.idonaldson.msscbeerservice.repositories.BeerRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 
+@RequiredArgsConstructor
 @Component
 public class BeerLoader implements CommandLineRunner {
 
@@ -22,9 +24,10 @@ public class BeerLoader implements CommandLineRunner {
     }
 
     private void loadBeerObjects() {
-        if(beerRepository.count() == 0 ){
+        if(beerRepository.count() == 0){
+
             beerRepository.save(Beer.builder()
-                    .beerName("Hop gun")
+                    .beerName("Mango Bobs")
                     .beerStyle("IPA")
                     .quantityToBrew(200)
                     .minOnHand(12)
@@ -33,14 +36,13 @@ public class BeerLoader implements CommandLineRunner {
                     .build());
 
             beerRepository.save(Beer.builder()
-                    .beerName("Electric Jellyfish")
-                    .beerStyle("IPA")
+                    .beerName("Galaxy Cat")
+                    .beerStyle("PALE_ALE")
                     .quantityToBrew(200)
                     .minOnHand(12)
                     .upc(337010000002L)
-                    .price(new BigDecimal("8.95"))
+                    .price(new BigDecimal("11.95"))
                     .build());
-
         }
     }
 }
